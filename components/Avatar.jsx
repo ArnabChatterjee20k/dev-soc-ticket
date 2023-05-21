46601d19e9d36ef9dce84ee3e195884c01bb46f5
arnab <arnabchatterjee.ac.2@gmail.com>
2023-05-21 14:50:07 +0530

    Feat -- Added Random Avatars generation

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -1,26 +1,38 @@
 import { BigHead } from "@bigheads/core";
+import { getRandomOptions } from "../src/utils/generateAvatar";
+import { useState } from "react";
 
-export const Avatar = () => (
-  <div className="avatar" style={{  position: "absolute", right: 38, top: 25 }}>
-    <BigHead
-      accessory="shades"
-      body="chest"
-      clothing="tankTop"
-      clothingColor="black"
-      eyebrows="angry"
-      eyes="wink"
-      facialHair="mediumBeard"
-      graphic="vue"
-      hair="short"
-      hairColor="black"
-      hat="none"
-      hatColor="green"
-      lashes="false"
-      lipColor="purple"
-      mask="true"
-      faceMask="true"
-      mouth="open"
-      skinTone="brown"
-    />
-  </div>
-);
+console.log(getRandomOptions())
+export const Avatar = () => {
+  const initialConfig = {
+    accessory: "shades",
+    body: "chest",
+    clothing: "tankTop",
+    clothingColor: "black",
+    eyebrows: "angry",
+    eyes: "wink",
+    facialHair: "mediumBeard",
+    graphic: "vue",
+    hair: "short",
+    hairColor: "black",
+    hat: "none",
+    hatColor: "green",
+    lashes: "false",
+    lipColor: "purple",
+    mask: "true",
+    faceMask: "true",
+    mouth: "open",
+    skinTone: "brown",
+  };
+  const currentOptions = getRandomOptions() || initialConfig
+  const [options, setOptions] = useState({...currentOptions,mask:true});
+  return (
+    <div
+      onClick={()=>setOptions(getRandomOptions())}
+      className="avatar cursor-pointer"
+      style={{ position: "absolute", right: 38, top: 25 }}
+    >
+      <BigHead {...options}/>
+    </div>
+  );
+};
